eeda044c9fa75159091fb2d974d751e8fc469547
Hosshii <sao_heath6147.wistre@icloud.com>
2021-12-10 22:24:05 +0900

    day03_2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,7 +1,9 @@
+use std::cmp::Ordering;
+
 fn main() {
     let input = include_str!("../input/input");
     let parsed = parse(input);
-    let solved = solve(&parsed);
+    let solved = solve(parsed);
     println!("{}", solved);
 }
 
@@ -12,27 +14,45 @@ fn parse(input: &str) -> Vec<Vec<bool>> {
         .collect()
 }
 
-fn solve(data: &[Vec<bool>]) -> u32 {
-    let mut gamma = Vec::new();
-    let cols = data[0].len();
-    for i in 0..cols {
-        let mut zeros = 0;
-        for row in data {
-            if !row[i] {
-                zeros += 1;
-            }
-        }
-        if data.len() as isize - 2 * zeros > 0 {
-            gamma.push(true);
-        } else {
-            gamma.push(false);
-        }
+fn solve(data: Vec<Vec<bool>>) -> u32 {
+    let mut gamma = data.clone();
+    let mut idx = 0;
+    while gamma.len() > 1 {
+        let b = match (gamma.len() as i32).cmp(&(count_bit(&gamma, idx) * 2)) {
+            Ordering::Greater => false,
+            Ordering::Less => true,
+            Ordering::Equal => true,
+        };
+        gamma = gamma.iter().cloned().filter(|v| v[idx] == b).collect();
+        idx += 1;
     }
-    let epsilon = gamma.iter().map(|c| !*c).collect::<Vec<bool>>();
-    let gamma_n = to_dec(&gamma);
-    let epsilon_n = to_dec(&epsilon);
+
+    let mut epsilon = data;
+    let mut idx = 0;
+    while epsilon.len() > 1 {
+        let b = match (epsilon.len() as i32).cmp(&(count_bit(&epsilon, idx) * 2)) {
+            Ordering::Greater => true,
+            Ordering::Less => false,
+            Ordering::Equal => false,
+        };
+        epsilon = epsilon.iter().cloned().filter(|v| v[idx] == b).collect();
+        idx += 1;
+    }
+
+    let gamma_n = to_dec(&gamma[0]);
+    let epsilon_n = to_dec(&epsilon[0]);
 
     gamma_n * epsilon_n
+}
+
+fn count_bit(data: &[Vec<bool>], n: usize) -> i32 {
+    let mut count = 0;
+    for row in data {
+        if row[n] {
+            count += 1;
+        }
+    }
+    count
 }
 
 fn to_dec(ipt: &[bool]) -> u32 {
