49e5bbec6e900850eb22234dc123f481037dae15
Hosshii <sao_heath6147.wistre@icloud.com>
2021-12-17 23:13:38 +0900

    day15_2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,4 +1,7 @@
-use std::collections::HashSet;
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashSet},
+};
 
 fn main() {
     let input = include_str!("../input/input");
@@ -9,38 +12,61 @@ fn main() {
 }
 
 fn parse(ipt: &str) -> Vec<Vec<u8>> {
-    ipt.lines()
+    let orig = ipt
+        .lines()
         .map(|line| {
             line.chars()
                 .map(|c| c.to_digit(10).unwrap() as u8)
                 .collect::<Vec<u8>>()
         })
-        .collect()
+        .collect::<Vec<_>>();
+    let mut result = Vec::new();
+    for y in 0..5 {
+        for rows in orig.iter() {
+            let mut row = Vec::new();
+            for x in 0..5 {
+                for col in rows {
+                    row.push(((col + x + y - 1) % 9) + 1);
+                }
+            }
+            result.push(row);
+        }
+    }
+    result
 }
 
 const DIRECTION: [(isize, isize); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct State {
+    pos: (usize, usize),
+    cost: u64,
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &State) -> Option<std::cmp::Ordering> {
+        self.cost.partial_cmp(&other.cost)
+    }
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &State) -> std::cmp::Ordering {
+        self.cost.cmp(&other.cost)
+    }
+}
 
 fn solve(map: &[Vec<u8>]) -> u64 {
     let mut v = vec![vec![u64::MAX; map[0].len()]; map.len()];
     v[0][0] = 0;
 
-    let mut cur = (0, 0);
-    let mut determined = HashSet::new();
+    let mut not_determined = BinaryHeap::new();
+    not_determined.push(Reverse(State {
+        pos: (0, 0),
+        cost: 0,
+    }));
 
-    let mut not_determined = HashSet::new();
-    for y in 0..map.len() {
-        for x in 0..map[0].len() {
-            not_determined.insert((x, y));
-        }
-    }
-
-    while !not_determined.is_empty() {
-        cur = find_min(&v, &not_determined);
-        // println!("{:?}", cur);
-        not_determined.remove(&cur);
-        determined.insert(cur);
-
-        let (cur_x, cur_y) = cur;
+    while let Some(Reverse(State { pos, cost })) = not_determined.pop() {
+        let (cur_x, cur_y) = pos;
         for (dx, dy) in DIRECTION {
             let (neighbor_x, neighbor_y) = (cur_x as isize + dx, cur_y as isize + dy);
             if neighbor_x < 0
@@ -52,11 +78,15 @@ fn solve(map: &[Vec<u8>]) -> u64 {
             }
             let (neighbor_x, neighbor_y) = (neighbor_x as usize, neighbor_y as usize);
 
-            let current_cost = v[cur_y][cur_x];
+            let current_cost = cost;
             let neighbor_cost_cur = v[neighbor_y][neighbor_x];
             let neighbor_cost_next = current_cost + map[neighbor_y][neighbor_x] as u64;
             if neighbor_cost_next < neighbor_cost_cur {
                 v[neighbor_y][neighbor_x] = neighbor_cost_next;
+                not_determined.push(Reverse(State {
+                    pos: (neighbor_x, neighbor_y),
+                    cost: neighbor_cost_next,
+                }));
             }
         }
     }
